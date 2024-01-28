5b76032f0719ac580d2e1296881c93b1da642b35
Damir <dense.neer@gmail.com>
2024-01-28 20:59:33 +0600

    categories web page and updated admin functions

---
+++ crud.js
@@ -0,0 +1,107 @@
+const { MongoClient } = require('mongodb');
+const wasi = require("wasi");
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'myProject';
+  
+
+const getAllCategories = async () => {
+    try{
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('categories');
+        const documents = await collection.find({}).toArray();
+        return documents;
+        // const titlesArray = documents.map(document => document.title);
+        // return titlesArray;
+    } catch (error){
+        console.error("Get All Categories Error:",error);
+        return null;
+    } finally {
+        await client.close;
+    }
+}
+const getAllCategoriesWithImg = async () => {
+    try{
+        await client.connect();
+        const db = client.db(dbName);
+        let collection = db.collection('categories');
+        const documents = await collection.find({}).toArray();
+        collection = db.collection('images')
+        for (const doc of documents) {
+            doc.image = await collection.findOne({_id: doc.imageID});
+        }
+        return  documents;
+    } catch (error){
+        console.error("Get All Categories Error:",error);
+        return null;
+    } finally {
+        await client.close;
+    }
+}
+async function addEquip(req, res){
+    try{
+        await client.connect();
+
+        const database = client.db(dbName);
+        let collection = database.collection('images');
+
+        const result = await collection.insertOne({
+            filename: req.file.filename,
+            path: req.file.path
+        });
+
+        console.log(`File uploaded with ID: ${result.insertedId}`);
+        console.log(req.body.productName);
+
+        collection = database.collection('equipments');
+        await collection.insertOne({
+            productName: req.body.productName,
+            description: req.body.description,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            category: req.body.category,
+            imageID: result.insertedId
+        })
+        res.send('File uploaded successfully!');
+    } catch (error){
+        console.error('Error uploading file', error);
+        res.status(500).send('Error uploading file.');
+    } finally {
+        await client.close();
+    }
+}
+async function addCategory(req,res){
+    try{
+        await client.connect();
+        const database = client.db(dbName);
+        let collection = database.collection('images');
+
+        const result = await collection.insertOne({
+            filename: req.file.filename,
+            path: req.file.path
+        });
+        collection = database.collection('categories')
+        await collection.insertOne({
+            title: req.body.categoryTitle,
+            imageID: result.insertedId
+        });
+
+        console.log(`Category add with ID: ${result.insertedId}`);
+    } catch (error) {
+        console.error('Error adding category:', error);
+        res.status(500).send('Error adding category.');
+    } finally {
+        await client.close();
+    }
+}
+
+
+
+
+module.exports.addEquip = addEquip;
+module.exports.getAllCategories = getAllCategories;
+module.exports.getAllCategoriesWithImg = getAllCategoriesWithImg;
+module.exports.addCategory = addCategory;
+module.exports.addEquip = addEquip;
--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
 const multer = require('multer');
 const path = require('path');
+const crudFunctions = require('./crud')
 
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-// Database Name
-const dbName = 'myProject';
 const app =  express();
-
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
@@ -23,106 +15,43 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 })
-app.get('/add-equip', async(req, res) => {
-    client.connect(); // vremeno
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
-    const collection = db.collection('categories');
-    const documents = await collection.find({}).toArray();
-    console.log('All Categories', documents);
-    const titlesArray = documents.map(document => document.title);
-    res.render('add-equip', {categories: titlesArray})
+app.get('/add-equip', async (req, res) => {
+    const titlesArray = await crudFunctions.getAllCategories();
+    res.render('add-equip', {categories: titlesArray.map(document => document.title)})
 })
 app.get('/add-category', (req, res) => {
-    res.render('add-category')
+    res.render('add-category');
 })
-app.get('/test', async (req, res) => {  
-    client.connect(); // vremeno
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
-    const collection = db.collection('categories');
-    const documents = await collection.find({}).toArray();
-    console.log('All Categories', documents);
-    const titlesArray = documents.map(document => document.title);  
-    res.render('test', {categories: titlesArray })
+app.get('/test', async (req, res) => {
+    const categories = await crudFunctions.getAllCategoriesWithImg();
+    res.render('test', {categories: categories })
 })
-app.post('/add-category-db', async (req, res) =>{
-    try{
-        await client.connect();
-        const database = client.db(dbName);
-        let collection = database.collection('categories');
-        const result = await collection.insertOne({
-            title: req.body.categoryTitle
-        });
-
-        console.log(`Category add with ID: ${result.insertedId}`);
-    } catch (error) {
-        console.error('Error adding category:', error);
-        res.status(500).send('Error adding category.');
-    } finally {
-        await client.close();
-    }
+app.post('/add-category-db',upload.single('image'), async (req, res) =>{
+    await crudFunctions.addCategory(req, res);
 })
-app.post('/upload', upload.single('image'), async (req, res) => {
-    try {
-        await client.connect();
-
-        const database = client.db(dbName);
-        let collection = database.collection('images');
-
-        const result = await collection.insertOne({
-            filename: req.file.filename,
-            path: req.file.path
-        });
-
-        console.log(`File uploaded with ID: ${result.insertedId}`);
-        console.log(req.body.productName);
-        
-        collection = database.collection('equipments');
-        await collection.insertOne({
-            productName: req.body.productName,
-            description: req.body.description,
-            price: req.body.price,
-            quantity: req.body.quantity,
-            category: req.body.category,
-            imageID: result.insertedId
-        })
-        res.send('File uploaded successfully!');
-    } catch (error) {
-        console.error('Error uploading file:', error);
-        res.status(500).send('Error uploading file.');
-    } finally {
-        await client.close();
-    }
+app.post('/add-equip-db', upload.single('image'), async (req, res) => {
+    await crudFunctions.addEquip(req, res)
 });
+app.get('/uploads/:imgname',(req, res) =>{
+    res.sendFile(__dirname + "/uploads/" + req.params.imgname)
+});
+app.get('/categories', async (req, res) => {
+    const categories = await crudFunctions.getAllCategoriesWithImg();
+    res.render('categories', {categories: categories });
+});
+
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server started: http://localhost:${PORT}`)
-})
-
-
-async function databaseConnection() {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
-    const collection = db.collection('categories');
-    const documents = await collection.find({}).toArray();
-
-        // Print or use the documents
-    console.log('All Categories', documents);
-
-    // the following code examples can be pasted here...
-    return 'done.';
-  }
-  
-databaseConnection()
+});
